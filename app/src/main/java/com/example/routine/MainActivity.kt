0a92a5b40fc5881ab5490a6e4ac7fb34e11169e4
shinseunghyuck <shinseunghyuck@gmail.com>
2025-07-30 05:36:21 +0000

    14:36 오류수정1

--- app/src/main/java/com/example/routine/MainActivity.kt
+++ app/src/main/java/com/example/routine/MainActivity.kt
@@ -31,6 +31,13 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 
 
 class MainActivity : ComponentActivity() {
