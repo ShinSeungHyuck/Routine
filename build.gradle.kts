815439dd2226e6510a5822b3f9dbe46e0c0d06de
ShinSeungHyuck <120080284+ShinSeungHyuck@users.noreply.github.com>
2025-08-06 15:04:21 +0900

    8/6 15:00

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id("com.android.application") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.21" apply false
     id("com.google.devtools.ksp") version "1.8.21-1.0.11" apply false
+    id("com.google.gms.google-services") version "4.4.3" apply false
 }
